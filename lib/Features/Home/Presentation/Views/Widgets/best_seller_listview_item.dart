8f1ae69d17eef6629af8fdf7f69ff960d49483ac
Ziad Alaa <ziadalaa955@gmail.com>
2024-12-12 15:32:46 +0200

    create custom book details app bar

--- lib/Features/Home/Presentation/Views/Widgets/best_seller_listview_item.dart
+++ lib/Features/Home/Presentation/Views/Widgets/best_seller_listview_item.dart
@@ -3,67 +3,73 @@ import 'package:bookly_app/Core/Utils/styles.dart';
 import 'package:bookly_app/Features/Home/Presentation/Views/Widgets/book_rating.dart';
 import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-      child: Row(
-        children: [
-          SizedBox(
-            height: 120,
-            child: Image.asset(
-              AssetsData.testImage,
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push('/BookDetailsView');
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        child: Row(
+          children: [
+            SizedBox(
+              height: 120,
+              child: Image.asset(
+                AssetsData.testImage,
+              ),
             ),
-          ),
-          const SizedBox(
-            width: 30,
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.57,
-                  child: Text(
-                    'The Jungle Book',
-                    style: Styles.style20.copyWith(
-                      fontFamily: kGtSectraFine,
-                      fontWeight: FontWeight.normal,
-                    ),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                const SizedBox(
-                  height: 3,
-                ),
-                Text(
-                  'Rudyard Kipling',
-                  style: Styles.style14.copyWith(
-                    color: const Color(0xffB7B6BC),
-                  ),
-                ),
-                const SizedBox(
-                  height: 3,
-                ),
-                const Row(
-                  children: [
-                    Text(
-                      '19.99 €',
-                      style: Styles.style20,
-                    ),
-                    Spacer(),
-                    BookRating(),
-                  ],
-                ),
-              ],
+            const SizedBox(
+              width: 30,
             ),
-          ),
-        ],
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.57,
+                    child: Text(
+                      'The Jungle Book',
+                      style: Styles.style20.copyWith(
+                        fontFamily: kGtSectraFine,
+                        fontWeight: FontWeight.normal,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  Text(
+                    'Rudyard Kipling',
+                    style: Styles.style14.copyWith(
+                      color: const Color(0xffB7B6BC),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  const Row(
+                    children: [
+                      Text(
+                        '19.99 €',
+                        style: Styles.style20,
+                      ),
+                      Spacer(),
+                      BookRating(),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/Features/Home/Presentation/Views/Widgets/book_details_view_body.dart
+++ lib/Features/Home/Presentation/Views/Widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -5,6 +6,41 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: kPadding),
+      child: Column(
+        children: [
+          SafeArea(
+            child: CustomBookDetailsAppBar(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomBookDetailsAppBar extends StatelessWidget {
+  const CustomBookDetailsAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.close),
+            iconSize: 28,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_cart_outlined),
+            iconSize: 28,
+          ),
+        ],
+      ),
+    );
   }
 }
